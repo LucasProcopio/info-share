5ed59216a93fa9e7e21521e548f6c4e0de2243b5
Lucas Henrique Procopio <procmode.dev@gmail.com>
2020-10-25 13:25:07 -0300

    :sparkles: feat: Add activity entity

---
+++ src/entities/Activities.ts
@@ -0,0 +1,24 @@
+class Activities {
+  id: number;
+
+  title: string;
+
+  body: string;
+
+  timecreated: Date;
+
+  timeupdated: Date;
+
+  authorID: number;
+
+  public: number;
+
+  contributorsIDs: string;
+
+  isExpired: number;
+
+  version: number;
+  // constructor
+}
+
+export default Activities;
